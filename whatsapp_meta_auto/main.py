ab09c9725b576eb996faad2742afb2a6b3795866
yashvikaushik <yashvikaushik2525@gmail.com>
2026-02-21 23:21:01 +0530

    recent update

--- whatsapp_meta_auto/main.py
+++ whatsapp_meta_auto/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from schemas import Message
 from services import whatsapp_messages
+from scheduler import start_scheduler
+
 app=FastAPI()
 
 
@@ -11,5 +13,12 @@ def home():
 @app.post('/send')
 def send_messages(payload:Message):
     return whatsapp_messages(payload.msg)
+
+@app.on_event("startup")
+def start_background_tasks():
+    start_scheduler()
     
-    
+@app.on_event("startup")
+def start_background_tasks():
+    print("Startup event triggered")
+    start_scheduler()
